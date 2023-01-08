b81bd2d1dafb8ce5e18d9d33a5ac6f369bc2a51a
Shiya Kohn <shiyaliebling@gmail.com>
2023-01-08 15:19:58 -0500

    Add working test

--- routes.test.js
+++ routes.test.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch');
 
 const { expect } = require('chai');
+
 const config = require('./config.json');
+
+const { HDate } = require('hebcal')
 
 const server = require('./server');
 
@@ -11,5 +14,11 @@ it('Should respond with hello world!', async () => {
   expect(response).to.property('status', 200);
   expect(await response.text()).eq('Hello world!');
 });
+
+it(`Should respond with today's date`, async () => {
+  const response = await fetch(`http://localhost:${config.port}/hebcal`).then(x => x.text());
+
+  expect(response).to.eq(new HDate().toString('h'));
+})
 
 after(() => server.close(() => 'Shutting down server'));
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const { HDate } = require('hebcal')
 const express = require('express');
 const app = express();
 const config = require('./config.json');
 
 app.get('/', (request, response) => response.send('Hello rold!'));
 
+app.get('/hebcal', (_, response) => response.send(new HDate().toString('h')))
 
 module.exports = app.listen(config.port, () => `Listening on ${config.port}`);
